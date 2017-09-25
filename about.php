73be6a9fe1115d2c47680183704e5c7b27251c84
Fastrok <32041982+Fastrok@users.noreply.github.com>
2017-09-25 16:59:20 +0300

    lesson 1

---
+++ about.php
@@ -0,0 +1,15 @@
+<?php
+$username = 'Андрей';
+$age = '32';
+$email ='kos@gg.ru';
+$city = 'Moscow';
+$ltd = 'sysadmin';
+?>
+<div>
+    <h1> Страница пользователя Андрея</h1>
+    <p>Имя <strong> <?= $username ?></p>
+    <p>Возраст <strong><?= $age ?></p>
+    <p>Адрес электронной почты <strong><?= $email ?></p>
+    <p>Город <strong><?= $city ?></p>
+    <p>О себе <strong><?= $ltd ?></p>
+</div>
